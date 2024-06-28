55f3b6e45350d66e68513441d8fb06c96723ad13
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-06-28 16:54:05 -0300

    Update collectionsHashMap.java

--- collectionsHashMap.java
+++ collectionsHashMap.java
@@ -1,1 +1,15 @@
 
+import java.util.HashMap;
+
+public class Main{
+	public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+	    map.put("UM ", 1);
+	    map.put("DOIS ", 2);
+	    map.put("TRÊS ", 3);
+	    
+	    for(String chave: map.keySet()){
+	        System.out.println("Chave " + chave + "Valor " + map.get(chave));
+	    }
+	}
+}

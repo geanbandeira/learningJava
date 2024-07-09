4dc7938d3924821890a04a7374b54f8ce70747be
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-07-08 22:02:08 -0300

    Update sistemaGerenciamentoJogadores.java

--- sistemaGerenciamentoJogadores.java
+++ sistemaGerenciamentoJogadores.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+import java.util.*;
 
+class Jogador{
+  private String nome;
+  private String id;
+  private String clube;
+  private String endereco;
+  private String dataNascimento;
+
+public Jogador(String nome, String id, String clube, String endereco, String dataNascimento){
+  this.nome = nome;
+  this.id = id;
+  this.clube = clube;
+  this.endereco = endereco;
+  this.dataNascimento = dataNascimento;
+}

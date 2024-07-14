6cf1963ddf7458877cd4a7468b96ee525f6ed350
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-07-13 23:34:23 -0300

    Update Customers.java

--- Customers.java
+++ Customers.java
@@ -15,15 +15,15 @@ class Customer{
         this.endereco = endereco;
         this.dataNascimento = dataNascimento;
     }
-    
+
     public String getNome(){
         return nome;
     }
-    
+
     public void setNome(String nome){
         this.nome = nome;
     }
-    
+
     public String getIn(){
         return in;
     }

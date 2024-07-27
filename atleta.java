31bff9831a0cc8fc659b51ea3f6ed4bf1dc1a746
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-07-27 13:44:21 -0300

    Create atleta.java

---
+++ atleta.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Atleta{
+  private String numDocumento;
+  private String nacionalidade;
+
+  public Atleta(String numDocumento, String nacionalidade){
+    
+  }
+}

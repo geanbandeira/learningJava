e80c0d7974e89f42ea20433503a8215dda42263f
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-07-29 20:31:22 -0300

    Create arrayBasico.java

---
+++ arrayBasico.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) {
+        ArrayList<String> nomes = new ArrayList<>();
+        
+        nomes.add("Gean");
+        nomes.add("18");
+        nomes.add("Rua 99");
+        
+        for(String nome : nomes){
+            System.out.println(nome);
+        }
+
+	}
+}

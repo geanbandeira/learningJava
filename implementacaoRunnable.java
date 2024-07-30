47a36f060893429c25ab70e0aa47dce51cb92ff0
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-07-30 18:24:46 -0300

    Create implementacaoRunnable.java

---
+++ implementacaoRunnable.java
@@ -0,0 +1,23 @@
+
+class MyRunnable implements Runnable{
+    public void run(){
+        for(int i = 0; i < 5; i++){
+            System.out.println(i);
+            try{
+                Thread.sleep(100);
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+} 
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Thread thread2 = new Thread(new MyRunnable());
+        thread2.start();
+        
+        
+    }
+}

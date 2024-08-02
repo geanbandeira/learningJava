0b9f060b0999a2d05f792a70b961a37ce9fcae32
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-08-02 17:30:59 -0300

    Update leituraEscritaArquivos.java

--- leituraEscritaArquivos.java
+++ leituraEscritaArquivos.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+//manipulando arquivos com FileReader e BufferedReader
+
 public class Main {
     public static void main(String[] args) {
         

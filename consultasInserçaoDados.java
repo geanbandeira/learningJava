1319c973cebd627af71485eb18acdbd422497597
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-08-04 11:09:37 -0300

    Update consultasInserçaoDados.java

--- consultasInserçaoDados.java
+++ consultasInserçaoDados.java
@@ -1,13 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main{
 	public static void main(String[] args) {
 
-        String jdbcURL = "jcbd:mysql://localhost:3306/database";
+        String jcbdURL = "jcbd:mysql://localhost:3306/database";
         String username = "root";
-        String password = "password"
+        String password = "password";
 
         String sql = "INSERT INTO users (username, email) VALUE (?, ?)";
         
+        try{
         Connection connection = DriverManager.getConnection(jcbdURL, username, password);
+        
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, "gean");
+        statement.setString(2, "gean@gg.com");
+        
+        int rows = statement.executeUpdate();
+        
+        if(rows > 0){
+            System.out.println("Novo usuario inserido com sucesso\n");
+        }
+        connection.close();
+        } catch(SQLException e){
+            e.printStackTrace();
+        
+        }
 	}
 }
